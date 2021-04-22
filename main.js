8cfd3f91133e58349267e7f4ab05ce83c02e2800
kabirvijaytandon <78803165+kabirvijaytandon@users.noreply.github.com>
2021-04-22 10:00:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+name_of_the_pizza_array = ["Hawaian pizza","chicken pizza","pepporni pizza","Queens Pizza"];
+
+function getmenu()
+{
+    document.getElementById('display_pizza').innerHTML = name_of_the_pizza_array;
+}
